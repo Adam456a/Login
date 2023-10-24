f690c2c61c972e110268013ea5af7bcf73732136
AdamNB <104266798+Adam456a@users.noreply.github.com>
2023-10-25 00:17:33 +0100

    create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+users = {"adam":"1234", "Admin":"Admin"}
+username = input("Please Type Your Username: ").lower()
+password = input("Please Type Your password: ").lower()
+
+if username in users and users[username] == password:
+        print("Login successful")
+
+else:
+    print ("username or password incorect please try again")
+    exit()
+
+print("!")
